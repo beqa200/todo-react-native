c684853474b7297a82a94f10981e15e72078608f
beqa <beqamaisuradze912@gmail.com>
2023-01-01 14:02:37 +0400

    add delete

--- components/TodoItem.tsx
+++ components/TodoItem.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import { TouchableOpacity, Text, StyleSheet, View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function TodoItem(props: TodoItemProps) {
   return (
-    <TouchableOpacity onPress={() => props.deleteTodos(props.item.key)}>
-      <Text style={styles.item}>{props.item.text}</Text>
+    <TouchableOpacity>
+      <View style={styles.wrapper}>
+        <MaterialIcons
+          name="delete"
+          size={24}
+          color="black"
+          onPress={() => props.deleteTodos(props.item.key)}
+        />
+        <Text style={styles.item}>{props.item.text}</Text>
+      </View>
     </TouchableOpacity>
   );
 }
@@ -12,10 +21,14 @@ export default function TodoItem(props: TodoItemProps) {
 const styles = StyleSheet.create({
   item: {
     padding: 16,
-    marginTop: 16,
+  },
+  wrapper: {
     borderColor: "#bbb",
     borderWidth: 1,
     borderStyle: "dashed",
     borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingLeft: 10,
   },
 });
